a5171790030744f0bea6525d2bbadcfc951510cc
thepigcat <benpospo@gmail.com>
2026-09-20 13:53:36 +0200

    tests

--- src/test.c
+++ src/test.c
@@ -1,27 +1,12 @@
 #include "../include/dynstr.h"
+#include "../include/str.h"
 #include "../include/log.h"
+#include "../include/array.h"
 
 int main(void) {
-  dyn_string_t str = {0};
-  dyn_string_init(&str, &HEAP_ALLOCATOR);
-
-  //dyn_string_copy_str(&str, "BALLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLZ");
-
-  log_info("Dyn str cap: %zu", str.capacity);
-
-  dyn_string_printf(&str, "lol %s", "hahaha");
-
-  log_info("Dyn str cap: %zu", str.capacity);
-
-  dyn_string_add_strf(&str, ". STOP %s :%d", "James", 3);
-
-  log_info("Dyn str cap: %zu, dyn str len: %zu", str.capacity, str.len);
-
-  log_info("STRING: %s", str.string);
-
-  dyn_string_remove_last_char(&str);
-
-  log_info("STRING: %s", str.string);
-
-  dyn_string_free(&str);
+  array_t(dyn_string_t) split = str_split("100, 200,30,10,", ',', &HEAP_ALLOCATOR);
+  dyn_string_t *split_val;
+  array_foreach(split, split_val) {
+    log_info("Val: %s", split_val->string);
+  }
 }
